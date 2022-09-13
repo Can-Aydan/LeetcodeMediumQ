189eb42a604421709fddc614e399e75662667346
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-13 10:14:08 +0300

    n3

---
+++ twoSumII167/Main.java
@@ -0,0 +1,14 @@
+package twoSumII167;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int target=9;
+		int[] numbers= {2,7,11,15};
+		System.out.println(Arrays.toString(sol.twoSum(numbers, target)));
+	}
+
+}
